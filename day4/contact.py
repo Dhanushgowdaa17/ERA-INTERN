ba280367320f032dc2d73d3af22111bd2f36b8f5
Dhanushgowdaa17 <cmdhanush6@gmail.com>
2026-02-05 11:41:04 +0530

    update

--- day4/contact.py
+++ day4/contact.py
@@ -1,17 +1,19 @@
 contacts = {
     "Rahul": "9876543210",
     "Sneha": "9123456789",
-    "Amit": "9988776655"
+    "Amit": "9988776655",
+    "Priya": "9012345678",
+    "Karan": "8899776655"
 }
 
-contacts["Priya"] = "9012345678"
 contacts["Rahul"] = "9999999999"
+contacts["Sneha"] = "9111111111"
 
-existing_contact = contacts.get("Sneha", "Contact not found")
+existing_contact = contacts.get("Amit", "Contact not found")
 missing_contact = contacts.get("Vikram", "Contact not found")
 
 print("Safe Lookups:")
-print("Sneha:", existing_contact)
+print("Amit:", existing_contact)
 print("Vikram:", missing_contact)
 
 print("\nContact List:")
